8e82139fa7e8922dad88ad547fc91a8a694fd0e0
oscar fernandez <oscar7fernandez@outlook.com>
2020-11-21 17:37:25 +0000

    JAVA connection to mongodb cluster

--- NbaMatchs/src/main/java/com/oscar/model/NbaDB.java
+++ NbaMatchs/src/main/java/com/oscar/model/NbaDB.java
@@ -1,22 +1,27 @@
 package com.oscar.model;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.*;
 
 public class NbaDB {
-	private MongoClient client;
-	private MongoDatabase db;
-	public NbaDB() { //Constructor of the class
-		this.client = new MongoClient();
-		this.db = client.getDatabase("nba");
-	}
-	
-	public MongoCollection<Document> getCollection(String collection_name){
-		return this.db.getCollection(collection_name);
-	}
-	public void insert(Document document) {
-		this.db.getCollection("team").insertOne(document);
-	}
+
+    private MongoClient client;
+    private MongoDatabase db;
+    private final MongoClientURI uri = new MongoClientURI(
+            "mongodb+srv://oscarfersan:QHZa5qJ532c2NSKg@nbadb.cp5yj.mongodb.net/nba?retryWrites=true&w=majority");
+    public NbaDB() { //Constructor of the class
+        this.client = new MongoClient(uri);
+        this.db = client.getDatabase("nba");
+    }
+
+    public MongoCollection<Document> getCollection(String collection_name) {
+        return this.db.getCollection(collection_name);
+    }
+
+    public void insert(Document document) {
+        this.db.getCollection("team").insertOne(document);
+    }
 }
--- NbaMatchs/src/main/java/com/oscar/nbamatchs/UpdateWindow.java
+++ NbaMatchs/src/main/java/com/oscar/nbamatchs/UpdateWindow.java
@@ -61,134 +61,130 @@ public class UpdateWindow extends javax.swing.JFrame {
 	 */
 	@SuppressWarnings("unchecked")
 	// <editor-fold defaultstate="collapsed" desc="Generated
-	// Code">//GEN-BEGIN:initComponents
-	private void initComponents() {
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
-		team_photo_2 = new javax.swing.JLabel();
-		team_photo_1 = new javax.swing.JLabel();
-		result_1 = new javax.swing.JTextField();
-		result_2 = new javax.swing.JTextField();
-		jComboBox1 = new javax.swing.JComboBox<>();
-		jComboBox2 = new javax.swing.JComboBox<>();
-		player_team_1 = new javax.swing.JTextField();
-		player_team_2 = new javax.swing.JTextField();
-		updateButton = new javax.swing.JButton();
-		nba_photo = new javax.swing.JLabel();
+        team_photo_2 = new javax.swing.JLabel();
+        team_photo_1 = new javax.swing.JLabel();
+        result_1 = new javax.swing.JTextField();
+        result_2 = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        player_team_1 = new javax.swing.JTextField();
+        player_team_2 = new javax.swing.JTextField();
+        updateButton = new javax.swing.JButton();
+        nba_photo = new javax.swing.JLabel();
 
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		
-		this.insertItems(jComboBox1, this.controller);
-		this.insertItems(jComboBox2, this.controller);
-		jComboBox1.addActionListener(new java.awt.event.ActionListener() {
-			@Override
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				String route = controller.obtainThumbnail((String)jComboBox1.getSelectedItem());
-				URL url;
-				try {
-					url = new URL(route);
-					//ImageIcon icon = new ImageIcon(ImageIO.read(url),"Team 1 Logo");
-					ImageIcon icon = new ImageIcon(new ImageIcon(ImageIO.read(url),"Team 1 Logo").getImage().getScaledInstance(team_photo_1.getWidth(), 
-							team_photo_1.getHeight(), Image.SCALE_DEFAULT));
-					team_photo_1.setIcon(icon);
-					jComboBox2.removeItem(jComboBox1.getSelectedItem());
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        this.insertItems(jComboBox1, this.controller);
+	this.insertItems(jComboBox2, this.controller);
+	jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+		@Override
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+			
+			String route = controller.obtainThumbnail((String)jComboBox1.getSelectedItem());
+			URL url;
+			try {
+				url = new URL(route);
+				//ImageIcon icon = new ImageIcon(ImageIO.read(url),"Team 1 Logo");
+				ImageIcon icon = new ImageIcon(new ImageIcon(ImageIO.read(url),"Team 1 Logo").getImage().getScaledInstance(team_photo_1.getWidth(), 
+						team_photo_1.getHeight(), Image.SCALE_DEFAULT));
+				team_photo_1.setIcon(icon);
+				jComboBox2.removeItem(jComboBox1.getSelectedItem());
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	});
+	jComboBox2.addActionListener(new java.awt.event.ActionListener() {
+		@Override
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+			String route = controller.obtainThumbnail((String)jComboBox2.getSelectedItem());
+			URL url;
+			try {
+				url = new URL(route);
+				//ImageIcon icon = new ImageIcon(ImageIO.read(url),"Team 1 Logo");
+				ImageIcon icon = new ImageIcon(new ImageIcon(ImageIO.read(url),"Team 2 Logo").getImage().getScaledInstance(team_photo_2.getWidth(), 
+						team_photo_2.getHeight(), Image.SCALE_DEFAULT));
+				team_photo_2.setIcon(icon);
+				jComboBox1.removeItem(jComboBox2.getSelectedItem());
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 				}
 			}
 		});
-		jComboBox2.addActionListener(new java.awt.event.ActionListener() {
-			@Override
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				String route = controller.obtainThumbnail((String)jComboBox2.getSelectedItem());
-				URL url;
-				try {
-					url = new URL(route);
-					//ImageIcon icon = new ImageIcon(ImageIO.read(url),"Team 1 Logo");
-					ImageIcon icon = new ImageIcon(new ImageIcon(ImageIO.read(url),"Team 2 Logo").getImage().getScaledInstance(team_photo_2.getWidth(), 
-							team_photo_2.getHeight(), Image.SCALE_DEFAULT));
-					team_photo_2.setIcon(icon);
-					jComboBox1.removeItem(jComboBox2.getSelectedItem());
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
 
-		updateButton.setText("Update");
-		updateButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				updateButtonActionPerformed(evt);
-			}
-		});
+	updateButton.setText("Update");
+	updateButton.addActionListener(new java.awt.event.ActionListener() {
+		public void actionPerformed(java.awt.event.ActionEvent evt) {
+			updateButtonActionPerformed(evt);
+		}
+	});
 
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-		getContentPane().setLayout(layout);
-		layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
-				.createSequentialGroup().addContainerGap()
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-						.addComponent(updateButton, javax.swing.GroupLayout.DEFAULT_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(layout.createSequentialGroup()
-								.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-										.addComponent(player_team_1, javax.swing.GroupLayout.Alignment.LEADING)
-										.addComponent(jComboBox1, 0, 182, Short.MAX_VALUE)
-										.addGroup(layout.createSequentialGroup()
-												.addComponent(team_photo_1, javax.swing.GroupLayout.PREFERRED_SIZE, 90,
-														javax.swing.GroupLayout.PREFERRED_SIZE)
-												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-												.addComponent(result_1, javax.swing.GroupLayout.PREFERRED_SIZE, 80,
-														javax.swing.GroupLayout.PREFERRED_SIZE)))
-								.addGap(26, 26, 26)
-								.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-										.addGroup(layout.createSequentialGroup()
-												.addComponent(result_2, javax.swing.GroupLayout.PREFERRED_SIZE, 78,
-														javax.swing.GroupLayout.PREFERRED_SIZE)
-												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-												.addComponent(team_photo_2, javax.swing.GroupLayout.PREFERRED_SIZE, 90,
-														javax.swing.GroupLayout.PREFERRED_SIZE))
-										.addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE,
-												Short.MAX_VALUE)
-										.addComponent(player_team_2))
-								.addGap(0, 0, Short.MAX_VALUE)))
-				.addContainerGap())
-				.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-						.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE).addComponent(nba_photo,
-								javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addGap(154, 154, 154)));
-		layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(layout
-				.createSequentialGroup().addContainerGap()
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(team_photo_2, javax.swing.GroupLayout.PREFERRED_SIZE, 90,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(team_photo_1, javax.swing.GroupLayout.PREFERRED_SIZE, 90,
-								javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addGroup(layout.createSequentialGroup().addGap(42, 42, 42).addGroup(layout
-								.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-								.addComponent(result_2, javax.swing.GroupLayout.PREFERRED_SIZE,
-										javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addComponent(result_1, javax.swing.GroupLayout.PREFERRED_SIZE,
-										javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-				.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(player_team_1, javax.swing.GroupLayout.PREFERRED_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(player_team_2, javax.swing.GroupLayout.PREFERRED_SIZE,
-								javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-				.addComponent(nba_photo, javax.swing.GroupLayout.DEFAULT_SIZE, 81, Short.MAX_VALUE)
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED).addComponent(updateButton)
-				.addContainerGap()));
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(updateButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(player_team_1, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jComboBox1, 0, 182, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(team_photo_1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(result_1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(26, 26, 26)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(result_2, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(team_photo_2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(player_team_2))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(nba_photo, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(154, 154, 154))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(42, 42, 42)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(result_2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(result_1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(team_photo_2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(team_photo_1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(player_team_1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(player_team_2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(nba_photo, javax.swing.GroupLayout.DEFAULT_SIZE, 81, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(updateButton)
+                .addContainerGap())
+        );
 
-		pack();
-	}// </editor-fold>//GEN-END:initComponents
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
 
 	/**
 	 * @param args the command line arguments
@@ -241,16 +237,16 @@ public class UpdateWindow extends javax.swing.JFrame {
 				(String) this.jComboBox2.getSelectedItem(), Integer.parseInt(this.result_1.getText()),
 				Integer.parseInt(this.result_2.getText()),this.player_team_1.getText(),this.player_team_2.getText());
 	}
-	// Variables declaration - do not modify//GEN-BEGIN:variables
-	private javax.swing.JComboBox<String> jComboBox1;
-	private javax.swing.JComboBox<String> jComboBox2;
-	private javax.swing.JLabel nba_photo;
-	private javax.swing.JTextField player_team_1;
-	private javax.swing.JTextField player_team_2;
-	private javax.swing.JTextField result_1;
-	private javax.swing.JTextField result_2;
-	private javax.swing.JLabel team_photo_1;
-	private javax.swing.JLabel team_photo_2;
-	private javax.swing.JButton updateButton;
-	// End of variables declaration//GEN-END:variables
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboBox2;
+    private javax.swing.JLabel nba_photo;
+    private javax.swing.JTextField player_team_1;
+    private javax.swing.JTextField player_team_2;
+    private javax.swing.JTextField result_1;
+    private javax.swing.JTextField result_2;
+    private javax.swing.JLabel team_photo_1;
+    private javax.swing.JLabel team_photo_2;
+    private javax.swing.JButton updateButton;
+    // End of variables declaration//GEN-END:variables
 }
